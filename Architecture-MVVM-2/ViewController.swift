31b8af77b5596ec5678f9d99c0d944c849b6aa30
Rus <de.mr44@gmail.com>
2022-02-20 18:16:07 +0500

    Initial Commit

---
+++ Architecture-MVVM-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Architecture-MVVM-2
+//
+//  Created by Ruslan on 20.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
